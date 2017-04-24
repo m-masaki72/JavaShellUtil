1a7b73905fd3a52ef0035b3374e108cdbad231c5
masaki mori <mementomori7272@gmail.com>
2017-04-24 17:02:50 +0900

    add recursive file find processing

--- javacc.sh
+++ javacc.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 echo "javaファイルをコンパイル後、実行します"
 echo "==========================="
-for i in *.java
-do
-    echo "-----------------------"
-    echo "FileName : " $i
-    echo "-----------------------"
-    javac $i
-    if [ $? -ne 0 ]; then
-        echo "ステータス：コンパイルエラー"
-	echo ""
-    else
-	grep "main" $i
-	if [ $? -eq 0 ]; then
-	    java ${i%.java} 
-	    if [ $? -eq 0 ]; then
-		echo "ステータス：実行完了"
-		echo ""
-	    else
-		echo "ステータス：実行エラー"
-		echo ""
-	    fi
-	else
-	echo "ステータス：コンパイル完了"
-	echo ""
-	fi
-    fi
-    echo "==========================="
-    echo ""
 
+find . -type f | while read FILE
+do
+    if [ $(echo $FILE | grep -e '.java') ]; then
+
+	echo "-----------------------"
+	echo "FileName : " $FILE
+	echo "-----------------------"
+
+	javac $FILE
+
+	echo ""
+	if [ $? -ne 0 ]; then
+            echo "ステータス：コンパイルエラー"
+	else
+	    grep "main" $FILE
+	    if [ $? -eq 0 ]; then
+		java ${FILE%.java} 
+		if [ $? -eq 0 ]; then
+		    echo "ステータス：実行完了"
+		else
+		    echo "ステータス：実行エラー"
+		fi
+	    else
+		echo "ステータス：コンパイル完了"
+	    fi
+
+	fi
+	echo ""
+	echo "==========================="
+	echo ""
+    fi
 done
